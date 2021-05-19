f2aefb02b1395563c096144b3dcb288605dd4356
faqihakih <faqihakih12@gmail.com>
2021-05-19 19:50:36 +0700

    1 - setup project

---
+++ app/src/main/java/com/akih/matarak/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.akih.matarak.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akih.matarak.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
