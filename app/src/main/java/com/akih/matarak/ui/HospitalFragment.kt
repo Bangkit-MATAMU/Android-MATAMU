f2e983d6c2b422b0df242d3a11f7b21cde5fbd48
faqihakih <faqihakih12@gmail.com>
2021-05-22 12:46:51 +0700

    4 - add google maps

--- app/src/main/java/com/akih/matarak/ui/HospitalFragment.kt
+++ app/src/main/java/com/akih/matarak/ui/HospitalFragment.kt
@@ -1,22 +1,75 @@
 package com.akih.matarak.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.akih.matarak.R
 import com.akih.matarak.databinding.FragmentHospitalBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HospitalFragment : Fragment() {
+class HospitalFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var binding: FragmentHospitalBinding
+    private var _binding: FragmentHospitalBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var mMaps: GoogleMap
+    var initial_marker    = "My Location"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHospitalBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentHospitalBinding.inflate(inflater, container, false)
+        val mFragment = childFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        mFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocation()
         return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMaps = googleMap
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        mMaps!!.setMyLocationEnabled(true)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let { currentLocation ->
+                val location = LatLng(currentLocation.latitude, currentLocation.longitude);
+                mMaps.addMarker(MarkerOptions().position(location).title(initial_marker));
+                mMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16f));
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
